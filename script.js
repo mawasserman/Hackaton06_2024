0e1fc096895c760f437db3e9ad9595e949e14934
mawasserman <wasserman.mv@gmail.com>
2024-06-06 18:45:25 +0300

    test

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+// draw canvas  - marcella
+
+// draw blocks - debi 
+
+// give colors to the blocks - debi
+
+// randowmly select a block
+
+// movement of the block
+//     automatic down
+//     side by side
+//     fast down
+
+// rotation of the block
+
+// collision detection
+
+// clear the row
+
+// game over
+
+// start
+
+// restart
+
+// score
+
+// level
+
+// speed
+
+// pause
+
+// sound
+
+// music
+
 
